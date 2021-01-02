90f9607c258663dd2938e26455b89b9886e6a83a
iamvickyav <vickyavw.10@gmail.com>
2021-01-02 20:07:50 +0530

    jenkinsSharedLib for Java

---
+++ vars/jenkinsForJava.groovy
@@ -0,0 +1,37 @@
+def call(String repoUrl, boolean publishArtifact=false) {
+    pipeline {
+        agent any
+        tools { 
+            maven 'Maven 3.5.0' 
+            jdk 'jdk8' 
+        }
+        stages {
+            stage("Tools initialization") {
+                sh "mvn --version"
+                sh "java -version"
+            }
+            stage("Checkout Code") {
+                steps {
+                    git branch: 'master',
+                        url: "${gitUrl}"
+                }
+            }
+            stage("Cleaning workspace") {
+                steps {
+                    sh "mvn clean"
+                }
+            }
+            stage("Running Testcase") {
+                steps {
+                    sh "mvn test"
+                }
+            }
+            stage("Packing Application") {
+                steps {
+                    sh "mvn package"
+                    sh "echo Build Successful"
+                }
+            }
+        }
+    }
+}
