51c80ae620d87f94134b03fe214af5bdb006ed0d
iamvickyav <vickyavw.10@gmail.com>
2021-01-04 19:51:10 +0530

    jenkinsLambdaTest branch name fixed

--- vars/jenkinsLambdaTest.groovy
+++ vars/jenkinsLambdaTest.groovy
@@ -14,7 +14,7 @@ def call(String repoUrl) {
                 }
             stage("Checkout Code") {
                 steps {
-                    git branch: 'master',
+                    git branch: 'main',
                     url: "${repoUrl}"
                 }
             }
