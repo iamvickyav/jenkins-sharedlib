ca18344ab0ca2256753d53b60afe0f89ed72e293
iamvickyav <vickyavw.10@gmail.com>
2021-01-04 19:48:39 +0530

    jenkinsLambdaTest added

---
+++ vars/jenkinsLambdaTest.groovy
@@ -0,0 +1,28 @@
+def call(String repoUrl) {
+    pipeline{
+        agent any
+        tools { 
+            maven 'Maven 3.5.0' 
+            jdk 'jdk8' 
+        }
+        stages {
+            stage("Tools initialization") {
+                steps {
+                        sh "mvn --version"
+                        sh "java -version"
+                    }
+                }
+            stage("Checkout Code") {
+                steps {
+                    git branch: 'master',
+                    url: "${repoUrl}"
+                }
+            }
+            stage("Running Testcase") {
+                steps {
+                    sh "mvn -Dusername=$LT_USERNAME -DaccessKey=$LT_ACCESS_KEY test"
+                }
+            }
+        }
+    }
+}
