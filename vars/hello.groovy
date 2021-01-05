d4ec8b4f897bf3e82d8a1487077546ec83c6801c
iamvickyav <vickyavw.10@gmail.com>
2021-01-05 09:39:08 +0530

    lambdatest shared library final

--- vars/hello.groovy
+++
@@ -1,3 +0,0 @@
-def call(String name = 'User') {
-    echo "Hello, ${name}."
-}
--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -1,48 +1,38 @@
-def call(String repoUrl, boolean publishArtifact=false) {
-    pipeline {
-        agent any
-        tools { 
-            maven 'Maven 3.5.0' 
-            jdk 'jdk8' 
-        }
-        stages {
-            stage("Tools initialization") {
-                steps {
-                    sh "mvn --version"
-                    sh "java -version"
-                }
-            }
-            stage("Checkout Code") {
-                steps {
-                    git branch: 'master',
-                        url: "${repoUrl}"
-                }
-            }
-            stage("Cleaning workspace") {
-                steps {
-                    sh "mvn clean"
-                }
-            }
-            stage("Running Testcase") {
-                when {
-                    expression {
-                        env.JOB_NAME.contains("test")
-                    }
-                }
-                steps {
-                    sh "mvn verify"
-                }
-            }
-            stage("Packing Application") {
-                when {
-                    expression {
-                        env.JOB_NAME.contains("deploy")
-                    }
-                }
-                steps {
-                    sh "mvn package -DskipTests"
-                }
-            }
-        }
-    }
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       tools {
+           maven 'Maven 3.5.0'
+           jdk 'jdk8'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
 }
